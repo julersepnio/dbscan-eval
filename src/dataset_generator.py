b54ae63fcd97129a4c66af0ff250382d0d30d367
Juler Sepnio <juler.sepnio@gmail.com>
2026-01-07 23:02:39 +0800

    feat(eda): add comprehensive eda on generated dataset

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -5,10 +5,7 @@ import matplotlib.pyplot as plt
 
 def generate_dataset():
   """
-  Generates time series dataset according to specifications.
-
-  Returns:
-    pd.DataFrame: DataFrame with columns 'timestamp', 'value', and 'is_anomaly'.
+  Generate time series dataset according to specifications.
   """
   np.random.seed(42)      # Set seed to ensure reproducibility
 
@@ -22,7 +19,7 @@ def generate_dataset():
   # Define seasonality
   amplitude = 50
   hour_in_decimal = timestamps.hour + timestamps.minute / 60 # Converts time to decimal representation (e.g 10:30 will become 10.5, 4:15 will become 4.25)
-  seasonality = amplitude * np.abs(np.sin(np.pi * hour_in_decimal / 24))
+  seasonality = amplitude * np.abs(np.sin(np.pi * hour_in_decimal / 24)) + 5
   noise = np.random.normal(0, 1, len(timestamps))
   values = (seasonality + noise).values
 
